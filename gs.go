7d061aca6b0d1a1b42b5cfb0817b038673ec627c
Alireza <26859725+tralireza@users.noreply.github.com>
2020-02-10 13:36:00 +0000

    Sorting 75m Sort Colors -> Dutch national flag problem :: Dijkstra

--- gs.go
+++ gs.go
@@ -5,3 +5,22 @@ import "log"
 func init() {
 	log.SetFlags(0)
 }
+
+// 75m Sort Colors
+func sortColors(nums []int) {
+	l, m, h := 0, 0, len(nums)-1
+
+	for m <= h {
+		switch nums[m] {
+		case 2:
+			nums[h], nums[m] = 2, nums[h]
+			h--
+		case 1:
+			m++
+		case 0:
+			nums[l], nums[m] = 0, nums[l]
+			l++
+			m++
+		}
+	}
+}
--- gs_test.go
+++ gs_test.go
@@ -1,7 +1,18 @@
 package Sorting
 
-import "log"
+import (
+	"log"
+	"testing"
+)
 
 func init() {
 	log.Print("> Sort")
+}
+
+// 75m Sort Colors
+func Test75(t *testing.T) {
+	for _, nums := range [][]int{{2, 0, 2, 1, 1, 0}, {2, 0, 1}} {
+		sortColors(nums)
+		log.Print(" -> ", nums)
+	}
 }
