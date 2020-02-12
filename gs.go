188e15f0fe55f0fd58d1479750794779f517379a
Alireza <26859725+tralireza@users.noreply.github.com>
2020-02-12 18:50:00 +0000

    128m Longest Consecutive Sequence -> Disjoint Set: O(n) Path Compression in FindSet(), Union by Rank

--- gs.go
+++ gs.go
@@ -1,6 +1,9 @@
 package Sorting
 
-import "log"
+import (
+	"fmt"
+	"log"
+)
 
 func init() {
 	log.SetFlags(0)
@@ -31,7 +34,78 @@ func sortColors(nums []int) {
 
 // 128m Longest Consecutive Sequence
 func longestConsecutive(nums []int) int {
-	lcs := 0
+	// -10^9 <= nums[i] <= 10^9
+	const nVal = int(1e9 + 7)
+
+	type DJVal struct{ root, rank int }
+
+	djSet := map[int]*DJVal{}
+
+	AddSet := func(n int) {
+		djSet[n] = &DJVal{root: n, rank: 0}
+	}
+	FindSet := func(n int) int {
+		if _, ok := djSet[n]; !ok {
+			return nVal
+		}
+
+		var Root func(n int) *DJVal
+		Root = func(n int) *DJVal {
+			N := djSet[n]
+			if N.root != n {
+				N.root = Root(N.root).root
+			}
+			return N
+		}
+
+		return Root(n).root
+	}
+	Union := func(x, y int) {
+		if FindSet(y) == nVal {
+			return
+		}
+		X, Y := djSet[FindSet(x)], djSet[FindSet(y)]
+		if X.rank >= Y.rank {
+			Y.root = X.root
+			if X.rank == Y.rank {
+				X.rank++
+			}
+		} else {
+			X.root = Y.root
+		}
+	}
+
+	for _, n := range nums {
+		AddSet(n)
+		Union(n, n+1)
+		Union(n, n-1)
+	}
+
+	Draw := func(m string) {
+		fmt.Print("DJS :: ")
+		for n, v := range djSet {
+			fmt.Printf("{%d:%d %d} ", n, v.root, v.rank)
+		}
+		fmt.Printf("/ [%s]\n", m)
+	}
+
+	Draw("")
+
+	lcs, fSet, Dup := 0, map[int]int{}, map[int]struct{}{}
+	for _, n := range nums {
+		if _, ok := Dup[n]; !ok { // [0 0 1 2 2] => [0 1 2] :: lcs:3 !5
+			Dup[n] = struct{}{}
+
+			r := FindSet(n)
+			fSet[r]++
+			lcs = max(fSet[r], lcs)
+		}
+	}
+
+	Draw("Path Compression")
+
+	log.Print("fSet :: ", fSet)
+
 	return lcs
 }
 
